8c8a77c3917e1bd4d09831028616bcba1e17b67b
MANUELA ALVES <manuela.alves7@etec.sp.gov.br>
2025-05-05 18:01:15 -0300

    Aula PW3 17-3-25

---
+++ PW3/Entidades/UsuarioEntidade.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PW3.Entidades
+{
+    public class UsuarioEntidade
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public bool Ativo { get; set; }
+
+   
+    }
+}
