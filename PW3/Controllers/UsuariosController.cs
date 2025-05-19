ee4b9c2fbb5a17ceaa42a63a11317318e2e5bdd1
MANUELA ALVES <manuela.alves7@etec.sp.gov.br>
2025-05-19 17:57:31 -0300

    19/5/25: added create, details and lista

--- PW3/Controllers/UsuariosController.cs
+++ PW3/Controllers/UsuariosController.cs
@@ -15,7 +15,7 @@ namespace PW3.Controllers
             using var connection = new MySqlConnection(connectionString);
             connection.Open();
 
-            var cmd = new MySqlCommand("SELECT id, email FROM usuarios", connection);
+            var cmd = new MySqlCommand("SELECT id, email FROM b", connection);
             using var reader = cmd.ExecuteReader();
 
             while (reader.Read())
@@ -28,6 +28,38 @@ namespace PW3.Controllers
             connection.Close();
             return View(model);
         
+
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        public IActionResult Lista()
+        {
+            List<UsuarioEntidade> model = new List<UsuarioEntidade>();
+
+            using var connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            var cmd = new MySqlCommand("SELECT id, email FROM b", connection);
+            using var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                UsuarioEntidade u = new UsuarioEntidade();
+                u.Id = reader.GetInt32("id");
+                u.Email = reader.GetString("email");
+                model.Add(u);
+            }
+            connection.Close();
+            return View(model);
 
         }
     }
